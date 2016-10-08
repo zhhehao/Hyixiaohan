1ca06170ed5957aa38bada7006673f3e8b6bd341
zhhehao <zh.hehao@gmail.com>
2016-10-08 15:24:01 +0800

    Oct 08 update

--- 0023/0023.py
+++ 0023/0023.py
@@ -3,9 +3,14 @@ import time
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def liuyan():
-	return render_template('liuyan.html')
+	if request.method == 'POST':
+		username = request.form['username']
+		message = request.form['message']
+		return render_template('liuyan.html', username=username, message=message)
+	else:
+		return render_template('liuyan.html')
 
 if __name__ == '__main__':
 	app.run()
