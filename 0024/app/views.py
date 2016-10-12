0999e9bd2f20a270866dbd24f81e4b667c5ec98e
zhhehao <zh.hehao@gmail.com>
2016-10-12 17:56:57 +0800

    update some desing

--- 0024/app/views.py
+++ 0024/app/views.py
@@ -5,4 +5,5 @@ from app import app
 @app.route('/index')
 def index():
 	user = {'nickname': 'Hao'}
+	flash('This is a flash message test.')
 	return render_template('index.html', user=user)
---
+++ 0024/config.py
@@ -0,0 +1,1 @@
+SECRET_KEY='this is secret key'
